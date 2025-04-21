e5cf063716ebdad3c982da426b0d12580de1a558
sandrinopatrulescu <sandryno_sandryno@yahoo.com>
2025-04-21 23:27:42 +0300

    add hide all button
    Took 9 minutes

--- index.js
+++ index.js
@@ -22,9 +22,21 @@ function rgbToHex(rgbString) {
   }).join('');
 }
 
+function forEachCard(cardConsumer) {
+  COLORS.forEach((color) => {
+    NUMBERS.forEach((number) => {
+      cardConsumer(color, number);
+    });
+  })
+}
+
 function updateAll(unhideAllButton) {
   unhideAllButton.disabled = isUsed.size === 0;
   unhideAllButton.style.cursor = unhideAllButton.disabled ? 'not-allowed' : 'pointer'
+
+  const hideAllButton = document.getElementById("hideAll");
+  hideAllButton.disabled = isUsed.size === NUMBER_OF_CARDS;
+  hideAllButton.style.cursor = hideAllButton.disabled ? 'not-allowed' : 'pointer'
 
   document.getElementById('handsPlayed').value = `${isUsed.size} / 4 = ${isUsed.size / 4}`;
   const cardsLeft = NUMBER_OF_CARDS - isUsed.size;
@@ -46,6 +58,34 @@ function unhideCard(cardName, img = undefined) {
   document.getElementById(`${cardName} handNumber`)?.remove();
 }
 
+function hideCard(cardName, img = undefined, imageContainer = undefined) {
+  if (img === undefined) {
+    img = document.getElementById(cardName);
+  }
+  if (imageContainer === undefined) {
+    imageContainer = document.getElementById(`${cardName} container`);
+  }
+
+  isUsed.add(cardName);
+  img.classList.add('cardHidden');
+  img.title = `Click to unhide ${cardName}`;
+
+  const handsPlayed = isUsed.size / 4;
+  const handsPlayedRounded = Math.ceil(handsPlayed);
+  img.style.border = `0.25rem solid ${HANDS_COLORS[handsPlayedRounded]}`;
+
+  const handNumberOverlay = document.createElement('div');
+
+  handNumberOverlay.id = `${cardName} handNumber`;
+  handNumberOverlay.innerText = `Hand ${handsPlayedRounded} (${handsPlayed})`;
+  handNumberOverlay.style.position = 'absolute';
+  handNumberOverlay.style.bottom = '0.5rem';
+  handNumberOverlay.style.left = '0.5rem';
+  handNumberOverlay.style.fontSize = '0.75rem';
+
+  imageContainer.appendChild(handNumberOverlay);
+}
+
 window.addEventListener('load', function () {
   const unhideAllButton = document.getElementById("unhideAll");
   unhideAllButton.addEventListener('click', () => {
@@ -56,6 +96,17 @@ window.addEventListener('load', function () {
     updateAll(unhideAllButton);
   });
   updateAll(unhideAllButton);
+
+  const hideAllButton = document.getElementById("hideAll");
+  hideAllButton.addEventListener('click', () => {
+    forEachCard((color, number) => {
+      const cardName = `${number} of ${color}`;
+      if (!isUsed.has(cardName)) {
+        hideCard(cardName);
+      }
+    });
+    updateAll(unhideAllButton);
+  });
 
   const backgroundColorPicker = document.getElementById('backgroundColorPicker');
   backgroundColorPicker.value = rgbToHex(getComputedStyle(document.body).getPropertyValue('background-color'));
@@ -77,60 +128,40 @@ window.addEventListener('load', function () {
 
   const flipCardAudio = new Audio('assets/flip-card.mp3');
 
-  COLORS.forEach((color) => {
-    NUMBERS.forEach((number) => {
-      const cardName = `${number} of ${color}`;
-      const hideCardString = `Click to hide ${cardName}`;
+  forEachCard((color, number) => {
+    const cardName = `${number} of ${color}`;
+    const hideCardString = `Click to hide ${cardName}`;
 
-      const imageContainer = document.createElement('div');
-      const img = document.createElement('img');
+    const imageContainer = document.createElement('div');
+    const img = document.createElement('img');
 
-      imageContainer.id = cardName + ' container';
-      imageContainer.style.position = 'relative';
-      imageContainer.style.display = 'inline-block';
-      imageContainer.style.border = '0.25rem solid trasnparent';
+    imageContainer.id = cardName + ' container';
+    imageContainer.style.position = 'relative';
+    imageContainer.style.display = 'inline-block';
+    imageContainer.style.border = '0.25rem solid trasnparent';
 
-      img.id = cardName;
-      img.classList.add('card');
-      img.src = `assets/cards/${color}_${number}.png`;
-      img.width = imgWidth;
-      img.alt = `Card ${cardName}`;
-      img.title = hideCardString;
-      img.style.display = 'block';
-      img.style.width = '100%';
-      img.style.height = 'auto';
-      imageContainer.appendChild(img);
-      cardsContainer.appendChild(imageContainer);
-      unhideCard(cardName, img);
+    img.id = cardName;
+    img.classList.add('card');
+    img.src = `assets/cards/${color}_${number}.png`;
+    img.width = imgWidth;
+    img.alt = `Card ${cardName}`;
+    img.title = hideCardString;
+    img.style.display = 'block';
+    img.style.width = '100%';
+    img.style.height = 'auto';
+    imageContainer.appendChild(img);
+    cardsContainer.appendChild(imageContainer);
+    unhideCard(cardName, img);
 
-      img.addEventListener('click', () => {
-        void flipCardAudio.play();
+    img.addEventListener('click', () => {
+      void flipCardAudio.play();
 
-        if (isUsed.has(cardName)) {
-          unhideCard(cardName, img);
-        } else {
-
-          isUsed.add(cardName);
-          img.classList.add('cardHidden');
-          img.title = `Click to unhide ${cardName}`;
-
-          const handsPlayed = isUsed.size / 4;
-          const handsPlayedRounded = Math.ceil(handsPlayed);
-          img.style.border = `0.25rem solid ${HANDS_COLORS[handsPlayedRounded]}`;
-
-          const handNumberOverlay = document.createElement('div');
-
-          handNumberOverlay.id = `${cardName} handNumber`;
-          handNumberOverlay.innerText = `Hand ${handsPlayedRounded} (${handsPlayed})`;
-          handNumberOverlay.style.position = 'absolute';
-          handNumberOverlay.style.bottom = '0.5rem';
-          handNumberOverlay.style.left = '0.5rem';
-          handNumberOverlay.style.fontSize = '0.75rem';
-
-          imageContainer.appendChild(handNumberOverlay);
-        }
-        updateAll(unhideAllButton);
-      });
+      if (isUsed.has(cardName)) {
+        unhideCard(cardName, img);
+      } else {
+        hideCard(cardName, img, imageContainer);
+      }
+      updateAll(unhideAllButton);
     });
   });
 });
